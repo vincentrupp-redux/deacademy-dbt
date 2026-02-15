b306e31c21b81851de6c8d23f4c00148fbe56e2a
Vincent Rupp <vincentrupp@gmail.com>
2026-02-15 22:16:46 +0000

    sales commit

---
+++ models/demo/sales.sql
@@ -0,0 +1,26 @@
+{{
+    config
+    (
+        materialized ='incremental',
+        incremental_strategy = 'append'
+    )
+}}
+with sales_src as
+(
+    select
+    SALE_ID,
+    SALE_DATE,
+    CUSTOMER_ID,
+    PRODUCT_ID,
+    QUANTITY,
+    TOTAL_AMOUNT,
+    CREATED_AT,
+    CURRENT_TIMESTAMP AS INSERT_DTS
+    FROM {{source('sales','SALES_SRC')}}
+
+
+    {% if is_incremental()%}
+    where CREATED_AT > (SELECT MAX(INSERT_DTS) FROM {{this}})
+    {% endif %}
+)
+SELECT * FROM sales_src
